78df86c9f240ef0c191c3e75a7f2059c688ae808
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2021-02-03 16:34:51 +0200

    add Homework 2

--- HW_2/main.js
+++ HW_2/main.js
@@ -1,34 +1,14 @@
-// Выведет строку 10
-console.log("" + 1 + 0);
-//-// Выведет число 1
-console.log("" - 1 + 0);
-// Выведет число 1
-console.log(true + false);
-//-// Будет ошибка
-console.log(6 / "3");
-// Выведет число 6
-console.log("2" * "3");
-// так как в выражении есть строка то оба плюса это конкатенация.
-//-// Выведет 45px
-console.log(4 + 5 + "px");
-// Выведет $45
-console.log("$" + 4 + 5);
-// Выведет число 2
-console.log("4" - 2);
-//-// Будет ошибка 
-console.log("4px" - 2);
-// Выведет infinity
-console.log(7 / 0);
-// Выведет строку " -9 5"
-console.log(" -9 " + 5);
-// Выведет число -14
-console.log(" -9 " - 5);
-// null преобразуется в 0. 
-//И тогда выведет число 1
-console.log(null + 1);
-//-// Выведет undefined
-console.log(undefined + 1);
-// \t -табуляция; \n - перенос строки.
-// В итоге строка без чисел и букв преобразуется в 0.
-// Выведет число -2
-console.log(" \t \n" - 2);
+// приоритет у логического И больше чем у ИЛИ
+
+// 'undefined' || null
+// 'undefined'
+console.log(10 && 'undefined' || null && -1);
+// 'false' || 0
+// 'false'
+console.log(' ' && true && 'false' || 0 && 7);
+// false || NaN || 0
+// 0
+console.log(false && null || NaN && Infinity || 0);
+// Infinity && NaN || 5
+// 1
+console.log(10 / -0 && 9 - '9cm' || 11 % 2);
