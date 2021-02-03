c39aa0197f2cad9136efd32b1fd5a3b311dc5a71
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2021-02-03 16:50:34 +0200

    change mistake

--- HW_2/main.js
+++ HW_2/main.js
@@ -9,6 +9,6 @@ console.log(' ' && true && 'false' || 0 && 7);
 // false || NaN || 0
 // 0
 console.log(false && null || NaN && Infinity || 0);
-// Infinity && NaN || 5
+// Infinity && NaN || 1
 // 1
 console.log(10 / -0 && 9 - '9cm' || 11 % 2);
