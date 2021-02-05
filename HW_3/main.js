3b5ea9d05d5d4b398511100dc22f4f365d1c43a8
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2021-02-05 22:01:24 +0200

    Changed HW_3 and Add_1

--- HW_3/main.js
+++ HW_3/main.js
@@ -2,71 +2,65 @@ let a;
 let operator;
 let b;
 let res;
-let control_1 = 1;
-let control_2 = 1;
-let control_3 = 1;
-
-while (1) {
-  // зациклено пока не получит правильное первое число
-  while (control_1) {
-    a = prompt("Введите первое число.");
-    if (a != null && !isNaN(+a) && isFinite(+a) && typeof +a == "number") {
-      control_1 = 0;
-
-      //зациклено пока не получит правильный знак
-      while (control_2) {
-        operator = prompt("Введите один знак(+ - / *)");
-        if (
-          operator === "+" ||
-          operator === "-" ||
-          operator === "*" ||
-          operator === "/"
-        ) {
-          control_2 = 0;
-
-          //зациклено пока не получит правильное второе число
-          while (control_3) {
-            b = prompt("Введите второе число.");
-            if (
-              b != null &&
-              !isNaN(+b) &&
-              isFinite(+b) &&
-              typeof +b == "number"
-            ) {
-              control_3 = 0;
-
-              //функция калькулятора
-              switch (operator) {
-                case "+":
-                  res = +a + +b;
-                  break;
-                case "-":
-                  res = +a - +b;
-                  break;
-                case "/":
-                  res = +a / +b;
-                  break;
-                case "*":
-                  res = +a * +b;
-                  break;
-                // не имеет смысла?
-                // default:
-                //   res = "Нет таких действий!";
-              }
-              alert(res);
-            } else {
-              alert("Вы или отменили запрос или неверно ввели второе число!");
-            }
+// функции
+function isNumber(v) {
+  return !isNaN(+v) && isFinite(+v) && typeof +v == "number";
+}
+function isCanceled(v) {
+  return v !== null;
+}
+function calculate(x, z, y) {
+  switch (z) {
+    case "+":
+      res = +x + +y;
+      break;
+    case "-":
+      res = +x - +y;
+      break;
+    case "/":
+      res = +x / +y;
+      break;
+    case "*":
+      res = +x * +y;
+      break;
+  }
+  return res;
+}
+function isOperator(v) {
+  return (
+    v === "+" || v === "-" || v === "*" || v === "/"
+  );
+}
+// проверка первого числа
+a = prompt("Введите первое число.");
+if (isCanceled(a)) {
+  if (isNumber(a)) {
+    // проверка знака
+    operator = prompt("Введите один знак(+ - / *)");
+    if (isCanceled(operator)) {
+      if (isOperator(operator)) {
+        // проверка второго числа
+        b = prompt("Введите второе число.");
+        if (isCanceled(b)) {
+          if (isNumber(b)) {
+            // функция калькулятора
+            calculate(a, operator, b);
+            alert(res);
+          } else {
+            alert("Вы неверно ввели второе число!");
           }
         } else {
-          alert("Вы или отменили запрос или неверно ввели знак!");
+          alert("Вы отменили запрос!");
         }
+      } else {
+        alert("Вы неверно ввели знак!");
       }
     } else {
-      alert("Вы или отменили запрос или неверно ввели первое число!");
+      alert("Вы отменили запрос!");
     }
+  } else {
+    alert("Вы неверно ввели первое число!");
   }
-  control_1 = 1;
-  control_2 = 1;
-  control_3 = 1;
+} else {
+  alert("Вы отменили запрос!");
 }
