c33373b099a0b317347f8b80abf7cc81087e962f
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2021-02-09 18:42:45 +0200

    HW_4

---
+++ CW_07.02/main.js
@@ -0,0 +1,13 @@
+let arr = [1, 12, 423, 55, 66, -87, -8];
+
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  console.log(element, i);
+  
+}
+
+// push(), pop() in end 1 element
+// unshift(), shift() in begining 1 element
+// concat([], [], ...) concatination arrays
+// reverse()
+
---
+++ HW_4/main.js
@@ -0,0 +1,73 @@
+let a;
+let operator;
+let b;
+let res;
+// функции
+function isNumber(v) {
+  return !isNaN(+v) && isFinite(+v) && typeof +v == "number" && v !== '';
+}
+function NotCanceled(v) {
+  return v !== null;
+}
+function calculate(x, z, y) {
+  switch (z) {
+    case "+":
+      res = +x + +y;
+      break;
+    case "-":
+      res = +x - +y;
+      break;
+    case "/":
+      res = +x / +y;
+      break;
+    case "*":
+      res = +x * +y;
+      break;
+  }
+  return res;
+}
+function isOperator(v) {
+  return (
+    v === "+" || v === "-" || v === "*" || v === "/"
+  );
+}
+
+// проверка первого числа
+a = prompt("Введите первое число.");
+if (NotCanceled(a)) {
+  if (isNumber(a)) {
+
+    // проверка знака
+    operator = prompt("Введите один знак(+ - / *)");
+    if (NotCanceled(operator)) {
+      if (isOperator(operator)){
+
+        // проверка второго числа
+        b = prompt("Введите второе число.");
+        if (NotCanceled(b)) {
+          if (isNumber(b)) {
+
+            // функция калькулятора
+            calculate(a, operator, b);
+            alert(res);
+
+          } else {
+            alert("Вы неверно ввели второе число!");
+          }
+        } else {
+          alert("Вы отменили запрос!");
+        }
+
+      }else {
+        alert("Вы неверно ввели знак!");
+      }
+    } else {
+      alert("Вы отменили запрос!");
+    }
+
+  } else {
+    alert("Вы неверно ввели первое число!");
+  }
+} else {
+  alert("Вы отменили запрос!");
+}
